d3ece9ac5ed0b6c0ccb0bcb044a60638e42f927f
BaptisteBR <baptiste.briot@gmail.com>
2019-12-23 19:26:43 +0100

    Change MD5 to SHA256

--- repo/UserRepository.js
+++ repo/UserRepository.js
@@ -1,22 +1,22 @@
-var md5 = require('../node_modules/md5');
+var sha256 = require('js-sha256');
 
 class UserRepository {
 
     constructor(dao) {
         this.dao = dao;
+        this.hash = sha256.create();
     }
 
     createTable() {
-        const sql = 'CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT, picture TEXT)';
-        return this.dao.run(sql);
+        return this.dao.run('CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT, picture TEXT)');
     }
 
     create(username, email, password, picture) {
-        return this.dao.run('INSERT INTO user (username, email, password, picture) VALUES (?, ?, ?, ?)', [username, email, md5(password), picture]);
+        return this.dao.run('INSERT INTO user (username, email, password, picture) VALUES (?, ?, ?, ?)', [username, email, this.hash.update(password).hex(), picture]);
     }
 
     getByUsernameAndPassword(username, password) {
-        return this.dao.get('SELECT * FROM user WHERE username = ? AND password = ?', [username, md5(password)]);
+        return this.dao.get('SELECT * FROM user WHERE username = ? AND password = ?', [username, this.hash.update(password).hex()]);
     }
 }
 
